1f35cf1c73acef3c9dc75a302b4ad6b0b0e66696
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-10 00:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  
+  for (var i = 1; i < musicians.length; i++){
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emptyArray;
+}
